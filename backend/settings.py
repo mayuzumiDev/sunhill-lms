2f17273633eb635eed39b44a72d27970fd407545
mayuzumi1272 <117078422+mayuzumiDev@users.noreply.github.com>
2024-12-08 22:12:28 +0800

    'update'

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oshm%x$+c-bgf@k)et9-x%_z1)_i90+*h1ef#bwb+o#zi%ay_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SITE_ID = 1
 
@@ -196,6 +196,11 @@ LOGGING = {
         },
     },
     'loggers': {
+        'user_admin': {  # This matches your app name
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
         '': {  # Root logger
             'handlers': ['console'],
             'level': 'INFO',
@@ -233,6 +238,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    'http://127.0.0.1:8000',
     'http://31.170.165.140',
     'http://sunhilllms.online',
 ]
